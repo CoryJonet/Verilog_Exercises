42e577782eb95171875ef8edc8e9fcdd2e37d1ba
Cory Jonet <CoryJonet@users.noreply.github.com>
2017-01-26 11:18:46 -0600

    HW5 Commit

---
+++ HW5/hw5_1/p1.asm
@@ -0,0 +1,9 @@
+lbi r1, 6
+lbi r2, 10
+lbi r3, 14
+lbi r6, 0
+add r1, r2, r3
+st r6, r1, 4
+ld r6, r1, 4
+ld r1, r6, 24
+add r5, r3, r1
---
+++ HW5/hw5_2/p2.asm
@@ -0,0 +1,11 @@
+lbi r0, 0
+lbi r3, 0
+lbi r4, 0
+lbi r5, 0
+lbi r6, 0
+//ld r0, r3, 4 
+beqz r0, 2
+add r3, r4, r5
+beqz r0, 2
+add r3, r2, r3
+add r3, r6, r6
